b63ac68e5e5d4bdb4e136cfdde34ff1659a43400
vishwasenthil <122578261+vishwasenthil@users.noreply.github.com>
2023-03-29 02:55:27 +0000

    fixed layout so sidebar and babylon viewport are side-by-side (grid layout)

--- react-app/src/components/Sidebar.js
+++ react-app/src/components/Sidebar.js
@@ -1,20 +1,15 @@
-import "./css/Sidebar.css"
+import "./css/SidebarStyles.css"
 
 function Sidebar() {
-    let containerStyle = {
-        height:"100vh",
-        width:"100vw",
-        display:`grid`,
-        gridTemplateColumns:`1fr 3fr`
-    };
     let sidebarStyle = {
-        gridColumns:`1/2`,
+        height:"100%",
+        width:"100%",
         background:`#ccc`,
         border:`3px solid black`,
         position:`relative`
     };
     return(
-        <div style={containerStyle}>
+        <div style={{height:"100vh", gridColumn:"1/2", gridRow:`1/2`}}>
             <aside style={sidebarStyle}>
                 <input type="text" style={{position:`absolute`, left:`0`, top:`0`, width:`100%`, boxSizing:`border-box`}}/>
             </aside>
