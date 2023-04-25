b1769a468aa6ac6e0d72cd606b2828c145358c6c
hapuum <85764055+hapuum@users.noreply.github.com>
2023-04-25 20:34:35 +0000

    camera switch implemented. needs to be separated from darkmode switch.

--- demoscript.js
+++ demoscript.js
@@ -1,20 +1,52 @@
 const canvas = document.getElementById('renderCanvas');
 const textInput1 = document.getElementById('input1');
 const textInput2 = document.getElementById('input2');
+const button = document.getElementById('button');
 const engine = new BABYLON.Engine(canvas, true);
 
 let lastRadius;
 let numMesh = 2;
 
+let currentCameraState = 'universal';
+let ArcRotateCamera;
+let UniversalCamera; 
+let camera;
 
+button.onclick = () => {
+    toggleCamera();
+}
+
+const toggleCamera = () => {
+    let myCanvas = myScene.getEngine().getRenderingCanvas();
+    let previousCamera = myScene.activeCamera;
+    myScene.activeCamera.detachControl();
+    if (currentCameraState === 'arcRotate') {
+        myScene.activeCamera = UniversalCamera; 
+        //need to figure out a way to change z and y axis... this works weirdly as of now.
+        let y = previousCamera.radius * Math.cos(previousCamera.beta);
+        let r = Math.abs(previousCamera.radius * Math.sin(previousCamera.beta));
+        let x = r * Math.cos(previousCamera.alpha);
+        let z = r * Math.sin(previousCamera.alpha);
+        UniversalCamera.position = new BABYLON.Vector3(x, y, z);
+        UniversalCamera.target = new BABYLON.Vector3(0,0,0);
+        currentCameraState = 'universal';
+    }
+    else {
+        myScene.activeCamera = ArcRotateCamera;
+        ArcRotateCamera.position = UniversalCamera.position;
+        currentCameraState = 'arcRotate';
+    }
+    myScene.activeCamera.attachControl(myCanvas);
+}
 
 const createScene = function() {
-    const scene = new BABYLON.Scene(engine);
+    let scene = new BABYLON.Scene(engine);
 
     //Custom Camera or switch back and forth between Universal and ArcRotate
     //const camera = new BABYLON.UniversalCamera('camera1', new BABYLON.Vector3(5, 5, 5), scene);
-    let camera = new BABYLON.ArcRotateCamera("camera", BABYLON.Tools.ToRadians(90), BABYLON.Tools.ToRadians(65), 10, BABYLON.Vector3.Zero(), scene);
-    camera.attachControl(canvas, true);
+    UniversalCamera = new BABYLON.UniversalCamera('universalCamera', new BABYLON.Vector3(5, 5, 5), scene);
+    ArcRotateCamera = new BABYLON.ArcRotateCamera('arcRotateCamera', BABYLON.Tools.ToRadians(90), BABYLON.Tools.ToRadians(65), 10, BABYLON.Vector3.Zero(), scene);
+    camera = UniversalCamera;
     lastRadius = camera.radius;
     //Refer to https://doc.babylonjs.com/features/featuresDeepDive/cameras/customizingCameraInputs for customized camera input
     
@@ -26,8 +58,8 @@ const createScene = function() {
     return scene;
 }
 
-const scene = createScene();
-const camera = scene.cameras[0];
+const myScene = createScene();
+toggleCamera();
 
 //id assigning system and storing textInput objects are to be dealt with while merging with vishwa's 
 textInput1.oninput = () => recreateMesh(textInput1.value, 1);
@@ -38,7 +70,7 @@ let textInputs = [textInput1, textInput2];
 
 //removes currently old graph and creates a new graph based on new informations
 const recreateMesh = (expression, id) => {
-    let currentGraph = scene.getMeshByName("graph" + id); 
+    let currentGraph = myScene.getMeshByName("graph" + id); 
     if(currentGraph != null) {
         currentGraph.dispose();
     }
@@ -98,18 +130,19 @@ const generateMeshFromFunction = (expression, id) => {
 const resizeThreshold = 30;
 
 engine.runRenderLoop(function() {
-    scene.render();
+    myScene.render();
     //resizeGraph();
 });
 
 const resizeGraph = () => {
-    
-    if (Math.abs(lastRadius - camera.radius) > resizeThreshold) {
-        lastRadius = camera.radius;
-        for (let i = 0; i < numMesh; i++) {
-            let currentTextInput = textInputs[i];
-            if (currentTextInput.value !== "") {
-                recreateMesh(currentTextInput.value, i + 1);
+    if (currentCameraState != 'universal') {
+        if (Math.abs(lastRadius - camera.radius) > resizeThreshold) {
+            lastRadius = camera.radius;
+            for (let i = 0; i < numMesh; i++) {
+                let currentTextInput = textInputs[i];
+                if (currentTextInput.value !== "") {
+                    recreateMesh(currentTextInput.value, i + 1);
+                }
             }
         }
     }
