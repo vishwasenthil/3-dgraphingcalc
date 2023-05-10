aa3acaca20d193b1375d964a493770ef49f7c9b1
owen-lai <124204269+owen-lai@users.noreply.github.com>
2023-05-10 00:33:54 +0000

    prototype

--- react-app/src/components/Sidebar.js
+++ react-app/src/components/Sidebar.js
@@ -1,7 +1,5 @@
 import "./css/SidebarStyles.css"
 import React, { useState, useEffect } from 'react';
-import {recreateMesh} from "./Graph"
-import KeyPadComponent from "./KeyPadComponent";
 
 function Sidebar(props) {
 
@@ -22,71 +20,24 @@ function Sidebar(props) {
         position:`relative`
     };
 
+    const [isOpen, barIsOpen] = useState(false);
+    const toggleSidebar = () => {
+        barIsOpen(!isOpen);
+    }
+    
+    
     const [theme, setTheme] = useState('light');
-    const toggleTheme = () => {
-        if (theme === 'light') {
-            setTheme('dark');
-        } else {
-            setTheme('light');
-        }
-    };
+    const toggleTheme = () => {
+        if (theme === 'light') {
+            setTheme('dark');
+        } else {
+            setTheme('light');
+        }
+    };
     useEffect(() => {
         document.body.className = theme;
-    }, [theme]);
-                
-    function handleInput(e) {
-        setResult(e.target.value); //sets result/value of textbox to updated version
-        recreateMesh(e.target.value, e.target.id);
-    }
+    }, [theme]);      
 
-
-    const [result, setResult] = useState(``);
-    let onClick = button => {
-        if(button === "C"){
-            reset()
-        }
-        else if(button === "DEL"){
-            backspace()
-        }
-        else {
-            var r2 = result + button; //fix to previous issue w/ accessing updated result value, using an extra variable
-            setResult(
-                r2
-            )
-            recreateMesh(r2, 10);
-        }
-    };
-let reset = () => {
-            var r2 = "";
-            setResult(
-                r2
-            )
-            recreateMesh(r2, 10);
-        };
-    
-        let backspace = () => {
-            if (result !== "") {
-                var r2 = result.slice(0, -1);
-                setResult(
-                    r2
-                )
-            }
-            recreateMesh(r2, 10);
-        };
-    function addInputField(e) {
-        console.log(e.key);
-        if(e.key === "Enter") {
-            setArr(arr=>{
-                return(
-                    [...arr,
-                        {id:arr.length+1}
-                    ]
-                );
-            });
-            console.log(arr.length);
-        }
-        return;
-    }
     return(
         <div style={{height:"100vh", gridColumn:"1/2", gridRow:`1/2`}}>
             <aside style={sidebarStyle}>
@@ -94,17 +45,18 @@ let reset = () => {
                     arr.map((input, index)=>{
                         return(
                             <div>
-                                <input onInput={handleInput} value={result} onKeyDown={addInputField} id={index} type="text" style={{ width:`100%`, boxSizing:`border-box`}}/>
+                                <input id={index} type="text" style={{ width:`100%`, boxSizing:`border-box`}}/>
                             </div>
                         );
                     })
             
                 }
-                <KeyPadComponent onClick={onClick}/>
+                
             </aside>
+            <button onClick={toggleSidebar}>Toggle Sidebar</button>
+            {isOpen &&  <Sidebar/> }
             <button onClick={toggleTheme} style={{position:`relative`, left:`0`, top:`0`}}> Change Light Mode </button>
         </div>
-        
     );
 }
 
